2d74c383fde47916e2499dab0c5e82faf782c6d9
AlexWilliams <aolaechea@unsa.edu.pe>
2021-11-20 13:44:58 -0500

    se registro algunos modelos para Pregunta

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+
+from .models import Categoria, Pregunta, Usuario
 User = get_user_model()
 
 # Register your models here.
 admin.site.register(User)
-
-
-
+admin.site.register(Pregunta)
+admin.site.register(Categoria)
+admin.site.register(Usuario)
