567865d128c8f87d47c1b06f16861cd2c4ec82a9
AlexWilliams <aolaechea@unsa.edu.pe>
2021-11-20 15:11:16 -0500

    Se asocio una url con la vista PreguntaCreateView

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ app_name="main"
 urlpatterns=[
     path("",views.preguntaConfiable,name="pregConfiable"),
     path('listar', views.PreguntaListView.as_view(), name = 'pregunta-list'),
+    path('crear/',views.PreguntaCreateView, name = 'pregunta-create'),
 ]
