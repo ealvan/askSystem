cf06dc791388a29f074552e2054acbae6a021cde
AlexWilliams <aolaechea@unsa.edu.pe>
2021-11-21 18:51:58 -0500

    se modifico urls.py para comentar una url

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,8 @@ app_name="main"
 
 urlpatterns=[
     path("",views.preguntaConfiable,name="pregConfiable"),
-    path("<int:question_id>/",views.singleQuestion,name="singleQuestion"),
+    #path("<int:question_id>/",views.singleQuestion,name="singleQuestion"),
     path('listar', views.PreguntaListView.as_view(), name = 'pregunta-list'),
     path('crear/',views.PreguntaCreateView, name = 'pregunta-create'),
-    #path('<int:pk>/', views.PreguntaDetailView, name = 'pregunta-detail'),
+    path('<int:pk>/', views.PreguntaDetailView, name = 'pregunta-detail'),
 ]
