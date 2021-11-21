444c4ddc57a3b4917aa9eaefae68dc8c0d9ed012
edsel <ealvan@unsa.edu.pe>
2021-11-21 18:57:54 -0500

    cambiando urls den urls.py

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,9 @@ from main import views
 app_name="main"
 
 urlpatterns=[
-    path("",views.preguntaConfiable,name="pregConfiable"),
+    path("confiable/",views.preguntaConfiable,name="pregConfiable"),
     path("<int:question_id>/",views.singleQuestion,name="singleQuestion"),
     path('listar', views.PreguntaListView.as_view(), name = 'pregunta-list'),
     path('crear/',views.PreguntaCreateView, name = 'pregunta-create'),
-    #path('<int:pk>/', views.PreguntaDetailView, name = 'pregunta-detail'),
+    path('table/<int:pk>/', views.PreguntaDetailView, name = 'pregunta-detail'),
 ]
