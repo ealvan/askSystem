8365c8400fb68eec76aff3e5f1cbed9de30abb06
Jeampier <jmoran@unsa.edu.pe>
2021-12-17 01:46:48 -0500

    Little changes

--- askSys/urls.py
+++ askSys/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("question/<int:question_id>/",views.singleQuestion, name="question"),
     path("question/ask",views.PreguntaCreateView, name="ask"),
     path("reply/<int:username>/<int:question_id>/",views.createReply,name="reply"),
-    path("resultados",views.resultados, name="resultados"),
+    path("search",views.resultados, name="search"),
     path("logout/", views.logout, name = "logout"),
     path("login/", views.login, name = "login"),
 ]
